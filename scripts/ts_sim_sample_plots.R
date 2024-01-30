21bf4044b8aa5eaacd45bb057a959521eb263c4a
dpweix <dpweix@gmail.com>
2024-01-30 13:04:13 -0600

    Alter plot_scenario function/add plots of all scenarios.

--- scripts/ts_sim_sample_plots.R
+++ scripts/ts_sim_sample_plots.R
@@ -11,25 +11,31 @@ library("patchwork")
 source(here("scripts", "data_generation.R"))
 source(here("scripts", "pl_fd_methods.R"))
 
-plot_scenario <- function(df, type = "color") {
+plot_scenario <- function(df, title = "", type = "color") {
+  df <- df |> 
+    mutate(index = 1:(2*n),
+           type = rep(c("IC", "OC"), each = n) |> as_factor())
+  
   if(type == "color") {
-    df |> 
+    plot <- df |> 
       pivot_longer(matches("x\\d"), names_transform = as_factor) |> 
       ggplot(aes(index, value, color = name)) +
       geom_line() + 
       geom_vline(xintercept = last(which(df$type == "IC")),
-                 color = "black", linetype = "dashed") +
-      labs(color = "")
+                 color = "black", linetype = "dashed")
   } else if(type == "facet") {
-    df |> 
+    plot <- df |> 
       pivot_longer(matches("x\\d"), names_transform = as_factor) |> 
       ggplot(aes(index, value)) +
       geom_line() + 
       geom_vline(xintercept = last(which(df$type == "IC")),
                  color = "black", linetype = "dashed") +
-      facet_wrap(~ name, scales = "free_x") +
-      labs(color = "")
+      facet_wrap(~ name, scales = "free_x")
   }
+  
+  plot + 
+    labs(title = title, color = "", x = "Observation Index", y = "") +
+    scale_color_viridis_d(begin = 0, end = .6)
 }
 
 
@@ -37,21 +43,31 @@ plot_scenario <- function(df, type = "color") {
 source(here("scripts", "sim_study_settings.R"))
 param_id <- 3
 
-# TS plot scenarios ----------------------------------------------------------
+# Generate data -----------------------------------------------------------
 df_s1 <-gen_dat_s1(n,   a[param_id])## |> plot_scenario()
 df_s2 <-gen_dat_s2(n,   b[param_id])## |> plot_scenario()
 df_s3 <-gen_dat_s3(n, rho[param_id])## |> plot_scenario()
-df_s4 <-gen_dat_s4(n,   b[param_id], data_only = FALSE)## |> plot_scenario()
-df_s5 <-gen_dat_s5(n,   b[param_id], data_only = FALSE)## |> plot_scenario()
-df_s6 <-gen_dat_s6(n,   a[param_id], data_only = FALSE)## |> plot_scenario()
-df_s7 <-gen_dat_s7(n,   b[param_id], data_only = FALSE)## |> plot_scenario()
-df_s8 <-gen_dat_s8(n, rho[param_id], data_only = FALSE)## |> plot_scenario()
-df_s9 <-gen_dat_s9(n,   a[param_id], data_only = FALSE)## |> plot_scenario()
-df_s10<-gen_dat_s10(n,  b[param_id], data_only = FALSE)## |> plot_scenario()
+df_s4 <-gen_dat_s4(n,   b[param_id])## |> plot_scenario()
+df_s5 <-gen_dat_s5(n,   b[param_id])## |> plot_scenario()
+df_s6 <-gen_dat_s6(n,   a[param_id])## |> plot_scenario()
+df_s7 <-gen_dat_s7(n,   b[param_id])## |> plot_scenario()
+df_s8 <-gen_dat_s8(n, rho[param_id])## |> plot_scenario()
+df_s9 <-gen_dat_s9(n,   a[param_id])## |> plot_scenario()
+df_s10<-gen_dat_s10(n,  b[param_id])## |> plot_scenario()
 
+# TS Plot of scenarios ----------------------------------------------------
+df_s1 |> plot_scenario(title = "Scenario 1")
+df_s2 |> plot_scenario(title = "Scenario 2")
+df_s3 |> plot_scenario(title = "Scenario 3")
+df_s4 |> plot_scenario(title = "Scenario 4")
+df_s5 |> plot_scenario(title = "Scenario 5")
+df_s6 |> plot_scenario(title = "Scenario 6")
+df_s7 |> plot_scenario(title = "Scenario 7")
+df_s8 |> plot_scenario(title = "Scenario 8")
+df_s9 |> plot_scenario(title = "Scenario 9")
+df_s10|> plot_scenario(title = "Scenario 10")
 
-apply_pl_fd(df_s1, n, method = "hawkins")
-
+# Scenario 1 --------------------------------------------------------------
 hawkins_s1 <- hawkins_2008(df_s1, colMeans(df_s1[1:n, ]), cov(df_s1[1:n, ]))
 mc_lasso_s1 <- MC_LASSO(df_s1, colMeans(df_s1[1:n, ]), cov(df_s1[1:n, ]))
 mc_comet_s1 <- MC_COMET(df_s1, colMeans(df_s1[1:n, ]), cov(df_s1[1:n, ]))
